2e2f8a465f5e3775466db644e44e54f5e067cf28
kaushal kishore <kkscientist15@gmail.com>
2018-06-13 14:51:34 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Reverse {
+    public static int reverse(int A) {
+        int sign=1;
+        if(A<0){
+            sign = -1;
+            A = A* sign;
+        }
+        long rev = 0;
+        while(A>0)
+        {
+            int rem = A%10;
+            rev = rev*10 + rem;
+            A = A/10;
+        }
+         if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE)
+            return 0;
+        else    
+            return (int)rev * sign;
+    }
+
+    
+
+
+    
+     public static void main(String []args){
+        System.out.println("Enter two numbers to find GCD");
+        Scanner sc = new Scanner(System.in);
+        int m= sc.nextInt();
+        int res = reverse(m);
+        
+        
+            System.out.println(res);
+     }
+     
+     
+}
