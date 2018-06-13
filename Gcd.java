a117d4712966e0a180c21247de977e88c42929c8
kaushal kishore <kkscientist15@gmail.com>
2018-06-13 14:02:36 +0530

    Create Gcd.java

---
+++ Gcd.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Gcd{
+
+// Recursive function to return gcd of a and b
+    static int GCD(int a, int b)
+    {
+        // Everything divides 0 
+        if (a == 0 || b == 0)
+            return 0;
+ 
+        // base case
+        if (a == b)
+            return a;
+ 
+        // a is greater
+        if (a > b)
+        return GCD(a-b, b);
+    return GCD(a, b-a);
+}
+    
+     public static void main(String []args){
+        System.out.println("Enter two numbers to find GCD");
+        Scanner sc = new Scanner(System.in);
+        int m= sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println(m+" "+n+"  :"+GCD(m,n));
+     }
+     
+     
+}
